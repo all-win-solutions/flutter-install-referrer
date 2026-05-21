483b4695db1e127ec687dda4a206bd574cd5f6ae
alejandro-all-win-software <alejandro@all-win-solutions.com>
2026-05-21 09:56:40 -0600

    3.0.0 Release

--- ios/flutter_install_referrer/Package.swift
+++ ios/flutter_install_referrer/Package.swift
@@ -10,11 +10,15 @@ let package = Package(
   products: [
     .library(name: "flutter-install-referrer", targets: ["flutter_install_referrer"])
   ],
-  dependencies: [],
+  dependencies: [
+    .package(name: "FlutterFramework", path: "../FlutterFramework")
+  ],
   targets: [
     .target(
       name: "flutter_install_referrer",
-      dependencies: [],
+      dependencies: [
+        .product(name: "FlutterFramework", package: "FlutterFramework")
+      ],
       resources: []
     )
   ]
